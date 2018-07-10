95d7997a48729e201650677ef1580bc283a62f4b
Dmitry Sergeevich Tarasenko <dmitry.tarasenko@lykke.com>
2018-07-10 10:21:46 +0300

    Add autofac registration LPDEV-657

--- client/Lykke.Service.PayPushNotifications.Client/AutofacExtension.cs
+++ client/Lykke.Service.PayPushNotifications.Client/AutofacExtension.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using Autofac;
+using Common;
+using Common.Log;
 using JetBrains.Annotations;
+using Lykke.Common.Log;
 using Lykke.HttpClientGenerator;
+using Lykke.Service.PayPushNotifications.Client.Publisher;
 
 namespace Lykke.Service.PayPushNotifications.Client
 {
@@ -23,10 +27,51 @@ namespace Lykke.Service.PayPushNotifications.Client
                 throw new ArgumentNullException(nameof(builder));
             if (settings == null)
                 throw new ArgumentNullException(nameof(settings));
+
             if (string.IsNullOrWhiteSpace(settings.ServiceUrl))
                 throw new ArgumentException("Value cannot be null or whitespace.", nameof(PayPushNotificationsServiceClientSettings.ServiceUrl));
 
             builder.RegisterClient<IPayPushNotificationsClient>(settings?.ServiceUrl, builderConfigure);
+        }
+
+        public static void RegisterPayPushNotificationPublisher(this ContainerBuilder builder,
+            RabbitMqPublisherSettings settings)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            if (settings == null)
+                throw new ArgumentNullException(nameof(settings));
+
+            builder.RegisterType<NotificationPublisher>()
+                .AsSelf()
+                .As<IStartable>()
+                .As<IStopable>()
+                .AutoActivate()
+                .SingleInstance()
+                .UsingConstructor(typeof(RabbitMqPublisherSettings), typeof(ILogFactory))
+                .WithParameter("settings", settings);
+        }
+
+        [Obsolete]
+        public static void RegisterPayPushNotificationPublisher(this ContainerBuilder builder,
+            RabbitMqPublisherSettings settings, ILog log)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            if (settings == null)
+                throw new ArgumentNullException(nameof(settings));
+
+            builder.RegisterType<NotificationPublisher>()
+                .AsSelf()
+                .As<IStartable>()
+                .As<IStopable>()
+                .AutoActivate()
+                .SingleInstance()
+                .UsingConstructor(typeof(RabbitMqPublisherSettings),typeof(ILog))
+                .WithParameter("settings", settings)
+                .WithParameter("log", log); ;
         }
     }
 }
