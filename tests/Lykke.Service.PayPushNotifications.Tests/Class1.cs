816871b51ec2dd87c0d2d98de1bb972e13cbecbb
Dmitry Sergeevich Tarasenko <dmitry.tarasenko@lykke.com>
2018-09-06 23:54:55 +0300

    Add logging LPDEV-838

---
+++ tests/Lykke.Service.PayPushNotifications.Tests/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lykke.Logs;
+using Lykke.Service.PayPushNotifications.Client.Publisher;
+using Xunit;
+
+namespace Lykke.Service.PayPushNotifications.Tests
+{
+    public class Class1
+    {
+        //    [Fact]
+        //    public async Task Test1()
+        //    {
+        //        try
+        //        {
+        //            var logFactory = LogFactory.Create();
+        //            using (var publisher = new NotificationPublisher(new RabbitMqPublisherSettings
+        //            {
+        //                ConnectionString = "amqp://lykke.history:lykke.history@rabbit-main.rabbits.svc.cluster.local:5672",
+        //                ExchangeName = "pay.push"
+        //            }, logFactory))
+        //            {
+        //                publisher.Start();
+
+        //                await publisher.PublishAsync(new NotificationMessage
+        //                {
+        //                    MerchantIds = new[] { "pem3" },
+        //                    Message = "test message"
+        //                });
+
+        //                publisher.Stop();
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            var a = 1;
+        //        }
+        //    }
+    }
+}
